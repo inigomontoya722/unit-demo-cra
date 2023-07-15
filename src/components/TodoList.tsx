6d99b2eae5b48022eac475fde58051f14afcb586
Urazov Dias <daniyar@chocotravel.com>
2023-07-15 10:21:33 +0300

    ci: eslint check added

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FC, useCallback } from "react";
+import React, { ChangeEvent, FC, useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { ApplicationState, addItem, setText } from "../store";
 import { TodoItem } from "./TodoItem";
